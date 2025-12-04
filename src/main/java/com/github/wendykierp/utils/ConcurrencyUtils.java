c401d921a1bb2d58adb599fe0482c587386a3bc6
Piotr Wendykier <piotr.wendykier@gmail.com>
2025-12-04 11:42:12 +0100

    - Repackaging: edu.emory.mathcs -> com.github.wendykierp. - Switched to Maven project. - Used JUnit for testing.

---
+++ src/main/java/com/github/wendykierp/utils/ConcurrencyUtils.java
@@ -0,0 +1,164 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * -- Innovative Computing Laboratory
+ * -- Electrical Engineering and Computer Science Department
+ * -- University of Tennessee
+ * -- (C) Copyright 2008
+ *
+ * Redistribution  and  use  in  source and binary forms, with or without
+ * modification,  are  permitted  provided  that the following conditions
+ * are met:
+ *
+ * * Redistributions  of  source  code  must  retain  the above copyright
+ *   notice,  this  list  of  conditions  and  the  following  disclaimer.
+ * * Redistributions  in  binary  form must reproduce the above copyright
+ *   notice,  this list of conditions and the following disclaimer in the
+ *   documentation  and/or other materials provided with the distribution.
+ * * Neither  the  name of the University of Tennessee, Knoxville nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS  SOFTWARE  IS  PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS''  AND  ANY  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A  PARTICULAR  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT NOT
+ * LIMITED  TO,  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA,  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY  OF  LIABILITY,  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF  THIS  SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package com.github.wendykierp.utils;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Concurrency utilities.
+ * 
+ * @author Piotr Wendykier (piotr.wendykier@gmail.com)
+ */
+public class ConcurrencyUtils {
+    private static ExecutorService THREAD_POOL = Executors.newCachedThreadPool(new CustomThreadFactory(
+            new CustomExceptionHandler()));
+
+    private static int NTHREADS = getNumberOfProcessors();
+
+    private static class CustomExceptionHandler implements Thread.UncaughtExceptionHandler {
+        public void uncaughtException(Thread t, Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class CustomThreadFactory implements ThreadFactory {
+        private static final ThreadFactory defaultFactory = Executors.defaultThreadFactory();
+
+        private final Thread.UncaughtExceptionHandler handler;
+
+        CustomThreadFactory(Thread.UncaughtExceptionHandler handler) {
+            this.handler = handler;
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = defaultFactory.newThread(r);
+            t.setUncaughtExceptionHandler(handler);
+            t.setDaemon(true);
+            return t;
+        }
+    };
+    
+    /**
+     * Shutdowns the thread pool.
+     */
+    public static void shutdown() {
+        THREAD_POOL.shutdown();
+    }
+
+
+    /**
+     * Submits a value-returning task for execution and returns a Future
+     * representing the pending results of the task.
+     * 
+     * @param <T>
+     * @param task
+     *            task for execution
+     * @return a handle to the task submitted for execution
+     */
+    public static <T> Future<T> submit(Callable<T> task) {
+        if (THREAD_POOL.isShutdown() || THREAD_POOL.isTerminated()) {
+            THREAD_POOL = Executors.newCachedThreadPool(new CustomThreadFactory(new CustomExceptionHandler()));
+        }
+        return THREAD_POOL.submit(task);
+    }
+
+    /**
+     * Submits a Runnable task for execution and returns a Future representing
+     * that task.
+     * 
+     * @param task
+     *            task for execution
+     * @return a handle to the task submitted for execution
+     */
+    public static Future<?> submit(Runnable task) {
+        if (THREAD_POOL.isShutdown() || THREAD_POOL.isTerminated()) {
+            THREAD_POOL = Executors.newCachedThreadPool(new CustomThreadFactory(new CustomExceptionHandler()));
+        }
+        return THREAD_POOL.submit(task);
+    }
+    
+    /**
+     * Returns the number of available processors
+     * 
+     * @return number of available processors
+     */
+    public static int getNumberOfProcessors() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    /**
+     * Returns the current number of threads.
+     * 
+     * @return the current number of threads.
+     */
+    public static int getNumberOfThreads() {
+        return NTHREADS;
+    }
+
+    /**
+     * Waits for all threads to complete computation.
+     * 
+     * @param futures
+     *            handles to running threads
+     */
+    public static void waitForCompletion(Future<?>[] futures) {
+        int size = futures.length;
+        try {
+            for (int j = 0; j < size; j++) {
+                futures[j].get();
+            }
+        } catch (ExecutionException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sets the number of threads
+     * 
+     * @param n
+     */
+    public static void setNumberOfThreads(int n) {
+        if (n < 1)
+            throw new IllegalArgumentException("n must be greater or equal 1");
+        NTHREADS = n;
+    }
+
+}
